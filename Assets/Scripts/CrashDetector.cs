3aa5fbccea6c963f17c85bd0fecfd00f65623049
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-06 01:32:48 -0600

    Scene Reload for Win and lose conditions

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class CrashDetector : MonoBehaviour
       Debug.Log("Bonked your head");
       other.gameObject.GetComponent<SurfaceEffector2D>().enabled = false;
       GetComponent<PlayerController>().enabled = false;
+
+      SceneManager.LoadScene(0);
     }
   }
 }
